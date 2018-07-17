1296b259e1d74ffca5e17b9beff29924b7c99354
Kari Bancroft <kariabancroft@gmail.com>
2018-07-17 16:55:10 -0700

    add task icons that match the status

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Incomplete from '../assets/images/Incomplete.svg';
 
 class Task extends Component {
   static propTypes = {
@@ -8,15 +7,20 @@ class Task extends Component {
     status: PropTypes.string.isRequired
   }
 
-  statusIcon = () => {
-    return <img src={ Incomplete }/>;
+  constructor() {
+    super();
+
+    this.images = {
+      completed: require('../assets/images/Completed.svg'),
+      incomplete: require('../assets/images/Incomplete.svg'),
+      locked: require('../assets/images/Locked.svg')
+    };
   }
 
   render() {
     return (
       <section className="task">
-        { this.props.status }
-        { this.statusIcon() }
+        <img src={ this.images[this.props.status] }/>
         { this.props.data.task }
       </section>
     );
